d917386db05fe6ff17ee036d0c64bc86f307eebf
VisheshRawal <vp2005rawal@gmail.com>
2023-10-12 00:34:04 +0530

    DSA-Notes

--- LL/DLL.cpp
+++ LL/DLL.cpp
@@ -9,26 +9,26 @@ struct Node {
 };
 
 // // insert node at the front
-// void insertFront(struct Node** head, int data) {
-//   // allocate memory for newNode
-//   struct Node* newNode = new Node;
+void insertFront(struct Node** head, int data) {
+  // allocate memory for newNode
+  struct Node* newNode = new Node;
 
-//   // assign data to newNode
-//   newNode->data = data;
+  // assign data to newNode
+  newNode->data = data;
 
-//   // make newNode as a head
-//   newNode->next = (*head);
+  // make newNode as a head
+  newNode->next = (*head);
 
-//   // assign null to prev
-//   newNode->prev = NULL;
+  // assign null to prev
+  newNode->prev = NULL;
 
-//   // previous of head (now head is the second node) is newNode
-//   if ((*head) != NULL)
-//     (*head)->prev = newNode;
+  // previous of head (now head is the second node) is newNode
+  if ((*head) != NULL)
+    (*head)->prev = newNode;
 
-//   // head points to newNode
-//   (*head) = newNode;
-// }
+  // head points to newNode
+  (*head) = newNode;
+}
 
 // insert a node after a specific node
 void insertAfter(struct Node* prev_node, int data) {
@@ -59,60 +59,60 @@ void insertAfter(struct Node* prev_node, int data) {
 }
 
 // // insert a newNode at the end of the list
-// void insertEnd(struct Node** head, int data) {
-//   // allocate memory for node
-//   struct Node* newNode = new Node;
+void insertEnd(struct Node** head, int data) {
+  // allocate memory for node
+  struct Node* newNode = new Node;
 
-//   // assign data to newNode
-//   newNode->data = data;
+  // assign data to newNode
+  newNode->data = data;
 
-//   // assign null to next of newNode
-//   newNode->next = NULL;
+  // assign null to next of newNode
+  newNode->next = NULL;
 
-//   // store the head node temporarily (for later use)
-//   struct Node* temp = *head;
+  // store the head node temporarily (for later use)
+  struct Node* temp = *head;
 
-//   // if the linked list is empty, make the newNode as head node
-//   if (*head == NULL) {
-//     newNode->prev = NULL;
-//     *head = newNode;
-//     return;
-//   }
+  // if the linked list is empty, make the newNode as head node
+  if (*head == NULL) {
+    newNode->prev = NULL;
+    *head = newNode;
+    return;
+  }
 
-//   // if the linked list is not empty, traverse to the end of the linked list
-//   while (temp->next != NULL)
-//     temp = temp->next;
+  // if the linked list is not empty, traverse to the end of the linked list
+  while (temp->next != NULL)
+    temp = temp->next;
 
-//   // now, the last node of the linked list is temp
+  // now, the last node of the linked list is temp
 
-//   // assign next of the last node (temp) to newNode
-//   temp->next = newNode;
+  // assign next of the last node (temp) to newNode
+  temp->next = newNode;
 
-//   // assign prev of newNode to temp
-//   newNode->prev = temp;
-// }
+  // assign prev of newNode to temp
+  newNode->prev = temp;
+}
 
 // // delete a node from the doubly linked list
-// void deleteNode(struct Node** head, struct Node* del_node) {
-//   // if head or del is null, deletion is not possible
-//   if (*head == NULL || del_node == NULL)
-//     return;
+void deleteNode(struct Node** head, struct Node* del_node) {
+  // if head or del is null, deletion is not possible
+  if (*head == NULL || del_node == NULL)
+    return;
 
-//   // if del_node is the head node, point the head pointer to the next of del_node
-//   if (*head == del_node)
-//     *head = del_node->next;
+  // if del_node is the head node, point the head pointer to the next of del_node
+  if (*head == del_node)
+    *head = del_node->next;
 
-//   // if del_node is not at the last node, point the prev of node next to del_node to the previous of del_node
-//   if (del_node->next != NULL)
-//     del_node->next->prev = del_node->prev;
+  // if del_node is not at the last node, point the prev of node next to del_node to the previous of del_node
+  if (del_node->next != NULL)
+    del_node->next->prev = del_node->prev;
 
-//   // if del_node is not the first node, point the next of the previous node to the next node of del_node
-//   if (del_node->prev != NULL)
-//     del_node->prev->next = del_node->next;
+  // if del_node is not the first node, point the next of the previous node to the next node of del_node
+  if (del_node->prev != NULL)
+    del_node->prev->next = del_node->next;
 
-//   // free the memory of del_node
-//   free(del_node);
-// }
+  // free the memory of del_node
+  free(del_node);
+}
 
 // print the doubly linked list
 void displayList(struct Node* node) {
@@ -131,10 +131,10 @@ int main() {
   // initialize an empty node
   struct Node* head = NULL;
 
-//   insertEnd(&head, 5);
-//   insertFront(&head, 1);
-//   insertFront(&head, 6);
-//   insertEnd(&head, 9);
+  insertEnd(&head, 5);
+  insertFront(&head, 1);
+  insertFront(&head, 6);
+  insertEnd(&head, 9);
 
   // insert 11 after head
   insertAfter(head, 11);
@@ -145,7 +145,7 @@ int main() {
   displayList(head);
 
   // delete the last node
-//   deleteNode(&head, head->next->next->next->next->next);
+  deleteNode(&head, head->next->next->next->next->next);
 
 //   displayList(head);
 }
